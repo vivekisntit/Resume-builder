7a78c6943fb694d6ae6e55d065f3b0e81b9b94be
vivekisntit <vivektgtc1901@gmail.com>
2026-02-21 17:06:55 +0530

    text formatting

--- src/cli/input_collector.py
+++ src/cli/input_collector.py
@@ -18,11 +18,11 @@ def get_experiences():
     while True:
         print(f"\n--- Experience {i} ---")
         exp={
-            "job": get_input("Enter job title"),
-            "level": get_input("Enter job level (Eg: Intern, Full-time)"),
-            "employer": get_input("Enter employer name"),
-            "date_range": get_input("Enter date range (Eg: Jan 2020 - Dec 2021)"),
-            "description": get_dynamic_list("experience description bullets")
+            "job": get_input("Enter job title [Eg: Software Developer, Data Analyst]"),
+            "level": get_input("Enter job level [Eg: Intern, Full-time]"),
+            "employer": get_input("Enter employer name [Eg: Blackrock, Microsoft]"),
+            "date_range": get_input("Enter date range [Eg: Jan 2020 - Dec 2021]"),
+            "description": get_dynamic_list("experience description bullets [one bullet at a time]")
         }
         experiences.append(exp)
         more=get_input(f"Experience {i+1}? (y/n)").lower()
@@ -37,10 +37,10 @@ def get_projects():
     while True:
         print(f"\n--- Project {i} ---")
         proj={
-            "pr_cr_name": get_input("Enter project name"),
+            "pr_cr_name": get_input("Enter project name]"),
             "timeframe": get_input("Enter project timefram [Eg: Jan 2020 - Dec 2021]"),
-            "description": get_dynamic_list("project description [One bullet at a time]"),
-            "framework": get_dynamic_list("the frameworks/libraries which you used in this project"),
+            "description": get_dynamic_list("project description [one bullet at a time]"),
+            "framework": get_dynamic_list("the frameworks/libraries which you used in this project [one at a time]"),
             "domain": get_input("Enter project domain (Eg: Web dev, App dev, ML engineer etc. [WRITE IN FULL FORM])")
         }
         projects.append(proj)
@@ -62,8 +62,8 @@ def build_resume():
 
         "education": [
             {
-                "school": get_input("Enter your school/college name"),
-                "grad_date": get_input("Enter your graduation year"),
+                "school": get_input("Enter your college name"),
+                "grad_date": get_input("Enter your graduation timeline [Eg: 2024 - 2028]"),
                 "degree": get_input("Enter degree you're pursuing/pursued (Eg: B.Tech in ECE [WRITE IN FULL FORM])"),
                 "coursework": get_dynamic_list("your coursework (Eg: WebDev, DBMS, DSA, ML, Data Science etc. [WRITE IN FULL FORM, one at a time])")
             }
@@ -73,9 +73,9 @@ def build_resume():
         "projects": get_projects(),
 
         "skills": {
-            "prog_lang": get_dynamic_list("mention any programming languages that you know [one at a time]"),
-            "frm_lib": get_dynamic_list("mention any frameworks/libraries/tools that you know [one at a time]"),
-            "duties": get_dynamic_list("mention any duties/soft skills you might have [Enter a sentence, one at a time]"),
+            "prog_lang": get_dynamic_list("any programming languages that you know [one at a time]"),
+            "frm_lib": get_dynamic_list("any frameworks/libraries/tools that you know [one at a time]"),
+            "duties": get_dynamic_list("any duties/soft skills you might have [Enter a sentence, one at a time]"),
         }
     }
     return resume_data
